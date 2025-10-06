7a53d0255f222a101e39d89c572fa477a7c8de10
Danylo Saikin <saydanya@gmail.com>
2025-10-06 19:28:36 +0300

    Cтворення окремого локатора для SEARCH_BUTTON

--- lesson_27/pages/tracking_page.py
+++ lesson_27/pages/tracking_page.py
@@ -9,6 +9,7 @@ class TrackingPage:
 
     INPUT_FIELD = (By.XPATH, '//input[@class="track__form-group-input"]')
     STATUS_TEXT = (By.CSS_SELECTOR, "div.header__status-text")
+    SEARCH_BUTTON = (By.ID, "np-number-input-desktop-btn-search-en")
 
     def __init__(self, driver):
         self.driver = driver
@@ -28,7 +29,7 @@ class TrackingPage:
 
     def click_search_button(self):
         search_button = self.wait.until(
-            EC.element_to_be_clickable((By.ID, "np-number-input-desktop-btn-search-en"))
+            EC.element_to_be_clickable(self.SEARCH_BUTTON)
         )
         search_button.click()
 
