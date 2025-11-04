79f52553946529a3c242da4418c27d6170054016
danylo.saikin <danylo.saikin@xenial.com>
2025-11-04 21:19:29 +0200

    Update conftest.py

--- lesson_31/conftest.py
+++ lesson_31/conftest.py
@@ -11,6 +11,10 @@ import time
 def browser():
     chrome_options = Options()
     chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--window-size=1920,1080')
     driver = webdriver.Chrome(options=chrome_options)
     driver.maximize_window()
     yield driver
