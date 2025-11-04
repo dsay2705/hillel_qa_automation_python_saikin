8d59bc82f32dec39d16bd918c527556fce81daa1
danylo.saikin <danylo.saikin@xenial.com>
2025-11-04 17:40:46 +0200

    remove Dynaconf

--- lesson_31/settings.py
+++ lesson_31/settings.py
@@ -1,15 +1,6 @@
-from dynaconf import Dynaconf
-from pathlib import Path
-from os.path import join
+from types import SimpleNamespace
 
-BASE_PATH = str(Path(__file__).parent)
-
-settings = Dynaconf(
-    envvar_prefix="DYNACONF",
-    settings_files=[
-        join(BASE_PATH, "base_settings.ini"),
-        join(BASE_PATH, "base_secrets.ini")
-    ],
-    load_dotenv=True,
-    environments=False
+settings = SimpleNamespace(
+    TRACKING_URL='https://tracking.novaposhta.ua/#/uk',
+    TRACKING_NUMBER="20451248324906"
 )
